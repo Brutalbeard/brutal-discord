9aa7dd2f8a91c5149f9b620ba41beb05a5ac113f
John Celoria <john.celoria@Johns-MBP.carolina.rr.com>
2019-03-07 19:54:38 -0500

    lop and sew!

--- src/commands/sew.ts
+++ src/commands/sew.ts
@@ -3,9 +3,9 @@ import getOrSetUser from '../lib/users'
 import db from "../lib/mongo-client";
 
 module.exports = {
-    name: 'lop',
-    description: 'Lop off someone else\'s appendage',
-    usage: "!lop @Anarkytt",
+    name: 'sew',
+    description: 'Sew a missing appendage back onto your friend. If you wanna.',
+    usage: "!sew @Anarkytt",
     async execute(message: Message, args: any) {
         getOrSetUser(message.author)
 
@@ -28,32 +28,41 @@ module.exports = {
 
         let user = await getOrSetUser(message.author)
         let mentionedUser = await getOrSetUser(message.mentions.users.array()[0])
-        console.log(mentionedUser)
+
+        if (user.id == mentionedUser.id) {
+            message.channel.send("Can't put your own appendages back on.")
+            return
+        }
 
         if (mentionedUser.appendages == undefined) {
             mentionedUser.appendages = appendages
         }
 
-        let index = Math.floor(Math.random() * mentionedUser.appendages.length)
-
-        let appendage = mentionedUser.appendages[index]
-        delete mentionedUser.appendages[index]
         let tempArr: string[] = []
-
-        for (let index in mentionedUser.appendages) {
-            if (mentionedUser.appendages[index] !== undefined) {
-                tempArr.push(mentionedUser.appendages[index])
+        for (let index in appendages) {
+            if (mentionedUser.appendages.includes(appendages[index])) {
+                continue
+            } else {
+                tempArr.push(appendages[index])
             }
         }
-        
-        console.log("Temp Lop Arr: ", tempArr)
-        mentionedUser.appendages = tempArr 
-        console.log("MEntioned USeR: ", mentionedUser)
-        
+
+        console.log("TempArr: ", tempArr)
+        if(tempArr.length == 0) {
+            message.channel.send("@" + mentionedUser.username + " didn't need anything sewed back on!")
+            return
+        }
+
+        let index = Math.floor(Math.random() * tempArr.length)
+
+        let appendage = tempArr[index]
+
+        mentionedUser.appendages.push(appendage)
+
         await db.users.updateOne({ id: mentionedUser.id }, {
-            $set: { appendages: tempArr }
+            $set: { appendages: mentionedUser.appendages }
         }).then(() => {
-            message.channel.send("@" + user.username + " just lopped off @" + mentionedUser.username + "'s " + appendage)
+            message.channel.send("@" + user.username + " just sewed @" + mentionedUser.username + "'s " + appendage + " back on. Old Frankenstein ass.")
         }).catch(e => {
             console.error(e)
             message.channel.send("Had an issue lopping stuff off...")
