908d693dda3bd31a2a82ec6cd063bb5b5fe345d1
John Celoria <jceloria@gmail.com>
2021-08-23 11:20:22 -0500

    ignore build folder

--- build/lib/dnd-client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = require("axios");
-var dnd = axios_1.default.create({
-    baseURL: "http://dnd5eapi.co/api"
-});
-exports.default = dnd;
--- build/lib/trivia-client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = require("axios");
-exports.triviaClient = axios_1.default.create({
-    baseURL: 'https://opentdb.com'
-});
