fdb6b97c33b71f4d9d9f017f1c82c0507ed6e4a4
John Celoria <jceloria@gmail.com>
2022-06-25 00:09:18 -0500

    couple bug fixes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM node:16
+FROM node:16-alpine
 
-WORKDIR /use/src/app
+WORKDIR /
 
 COPY . .
 
--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -3,7 +3,7 @@ import {getOrSetUser} from "../lib/users";
 module.exports = {
     name: 'interactionCreate',
     execute(interaction) {
-        console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+        console.log(`${interaction.user.tag || "Someone"} triggered an interaction.`);
         getOrSetUser(interaction.user)
             .catch(e => {
                 console.error(e);
